ef61497f69b1219a101ebd4f7b660d80a2a3a7e6
wjh <535944211@qq.com>
2018-10-16 17:31:18 +0800

    修改

--- src/main/java/com/lz/web/controller/ExcelController.java
+++ src/main/java/com/lz/web/controller/ExcelController.java
@@ -1,13 +1,9 @@
 package com.lz.web.controller;
 
-import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,6 +18,7 @@ public class ExcelController {
 		System.out.println("-------");
 		try {
 			InputStream is = myFile.getInputStream();
+			System.out.println(is);
 			String fileName = myFile.getOriginalFilename();
 			System.out.println(fileName);
 			if(fileName.endsWith("xls")) {
@@ -29,6 +26,7 @@ public class ExcelController {
 				parseExcel(hw);
 			}else if (fileName.endsWith("xlsx")){
 				XSSFWorkbook xw = new XSSFWorkbook(is);
+				System.out.println(xw);
 				parseExcel(xw);
 			}
 		} catch (IOException e) {
@@ -51,11 +49,55 @@ public class ExcelController {
 					continue;
 				}
 				short lastCellNum = row.getLastCellNum();
+				System.out.println("LastCellNum"+lastCellNum);
 				for (int j = 0; j < lastCellNum; j++) {
 					Cell cell = row.getCell(j);
+					System.out.println("cell"+cell);
+					System.out.println(cell.getCellType());
+					System.out.println(CellType.STRING);
+					cell.getStringCellValue();
 					System.out.println(cell.getStringCellValue());
 				}
 			}
 		}
 	}
+	/*private static String getValue(Cell xssfCell, Workbook workbook) {
+		String value = "";
+		switch (xssfCell.getCellType()) {
+			case Cell.CELL_TYPE_STRING:
+				value = String.valueOf(xssfCell.getRichStringCellValue()
+						.getString());
+				System.out.print("|");
+				break;
+			case Cell.CELL_TYPE_NUMERIC:
+				if (DateUtil.isCellDateFormatted(xssfCell)) {
+					value = String.valueOf(String.valueOf(xssfCell
+							.getDateCellValue()));
+				} else {
+					value = String.valueOf(xssfCell.getNumericCellValue());
+				}
+				System.out.print("|");
+				break;
+			case Cell.CELL_TYPE_BOOLEAN:
+				value = String.valueOf(xssfCell.getBooleanCellValue());
+				System.out.print("|");
+				break;
+			// 公式,
+			case Cell.CELL_TYPE_FORMULA:
+				// 获取Excel中用公式获取到的值,//=SUM(P4-Q4-R4-S4)Excel用这种类似的公式计算出来的值用POI无法获取，要想获取的话，就必须一下操作
+				FormulaEvaluator evaluator = workbook.getCreationHelper()
+						.createFormulaEvaluator();
+				evaluator.evaluateFormulaCell(xssfCell);
+				CellValue cellValue = evaluator.evaluate(xssfCell);
+				value = String.valueOf(cellValue.getNumberValue());
+				break;
+			case Cell.CELL_TYPE_ERROR:
+				value = String.valueOf(xssfCell.getErrorCellValue());
+				break;
+			default:
+		}
+		return value;
+	}
+*/
+
 }
